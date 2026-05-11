a6a230fbe72ebefddd47f7131176d30f774ec919
itsmebencebudai <bencebudai2004@gmail.com>
2026-05-11 19:11:21 +0200

    Update access key in access-config.js
    Replace the access identifier hash for the "AlphaInvestMedia redesign" entry in project/access-config.js. The mapping key was updated to a new hash value; no other changes were made.

--- project/access-config.js
+++ project/access-config.js
@@ -1,5 +1,5 @@
 window.PROJECT_ACCESS = {
-  "186474c1f2c2f735a54c2cf82ee8e87f2a5cd30940e280029363fecedfc5328c": {
+  "cc0a6cca98ff22c439f33f7fc21ab46e19098097f86b1780fda60148f3db885d": {
     "title": "AlphaInvestMedia redesign",
     "href": "AlphaInvestMedia_redesign/"
   }
